222e5c69c068b11c5f84a3306111a4b75342e956
shorinami <shorinami@atombuilt.com>
2023-11-30 21:15:01 +0200

    [ci skip] Add AtomBuilt banner const to commons

--- commons/src/main/kotlin/Extensions.kt
+++ commons/src/main/kotlin/Extensions.kt
@@ -1,3 +1,24 @@
 package com.atombuilt.atomkt.commons
 
+public const val ATOMBUILT_BANNER: String = """Developed by AtomBuilt.
 
+         666              66666         
+     666666664444     444446666666      
+    66666   444445555554444446 6666     
+    666         5555555          666    
+    666       522222222555       666    
+    66444   55222     22225     4666    
+    664445  222    22    2255   446     
+    64445   2   22333322  22555                 This software has been developed by AtomBuilt.
+      4455      33    33    5555                Visit our website at https://atombuilt.com.
+      445552   233    332   25544               For inquiries, please contact us at contact@atombuilt.com.
+     44455522   23333332   2555444              All rights to this software are held by the respective owners.
+     644  55222    22    2225  4446     
+    664     52222      22225    4466    
+    666       5     222225       666    
+    666          5555555         666    
+    6666     44455555554444  666666     
+     6666666644444    4444466666666     
+        66666              666666
+
+"""
